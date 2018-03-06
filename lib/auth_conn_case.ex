086e5191a27cbad807ee4d15c5f33582d90dca4f
Adrián Quintás <adrian@heyorbit.com>
2018-03-06 17:48:45 +0100

    Missing module fix

--- lib/auth_conn_case.ex
+++ lib/auth_conn_case.ex
@@ -10,15 +10,22 @@ defmodule JwtTestUtils.AuthConnCase do
       @fake_user_id "user@fake.is"
       @auth_header "authorization"
 
+      alias JwtTestUtils.JwtMocker
+
       @doc """
       Returns a Plug.Conn with a injected header with a JWT token injected.
 
       By default returns always the same token in the "authorization" header.
       """
-      @spec get_claim(String.t(), String.t()) :: {Plug.Conn.t()}
+      @spec build_authed_conn(String.t(), String.t()) :: {Plug.Conn.t()}
       def build_authed_conn(user_id \\ @fake_user_id, auth_header \\ @auth_header) do
         jwt = JwtMocker.generate_json_token(user_id)
-        conn = Plug.Adapters.Test.Conn.conn(%Conn{}, :get, "/", nil)
+
+        conn =
+          Plug.Adapters.Test.Conn.conn(%Plug.Conn{}, :get, "/", nil)
+          |> Plug.Conn.put_private(:plug_skip_csrf_protection, true)
+          |> Plug.Conn.put_private(:phoenix_recycled, true)
+
         Plug.Conn.put_req_header(conn, auth_header, jwt)
       end
     end
